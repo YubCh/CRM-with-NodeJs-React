52f9cbcdf0a40e330408790e7dcf3f490ada2916
Yu <ge75qeb@mytum.de>
2022-12-19 16:55:35 +0100

    Mui Table

--- management/src/App.js
+++ management/src/App.js
@@ -1,63 +1,17 @@
 import React, {Component} from 'react';
+import Customer, { MuiTable } from './components/Customer';
 import './App.css';
-import Customer from './components/Customer';
 
 
-const customers = [{
-  'id': 1,
-  'image': 'https://placeimg.com/64/64/1',
-  'name': 'Max',
-  'birthday': '1',
-  'gender': 'Male',
-  'job': 'Student'
-},
-{
-  'id': 2,
-  'image': 'https://placeimg.com/64/64/2',
-  'name': 'Emma',
-  'birthday': '2',
-  'gender': 'Female',
-  'job': 'Student'
-},
-{
-  'id': 3,
-  'image': 'https://placeimg.com/64/64/3',
-  'name': 'Van',
-  'birthday': '3',
-  'gender': 'Female',
-  'job': 'Student'
-},
-{
-  'id': 4,
-  'image': 'https://placeimg.com/64/64/4',
-  'name': 'Grief',
-  'birthday': '4',
-  'gender': 'Male',
-  'job': 'Student'
-},
-]
+
 
 
 function App() {
-  return (
-    <div>
-      {
-        customers.map(each => {
-          return(
-            <Customer
-            key={each.id}
-            id={each.id}
-            image={each.image}
-            name={each.name}
-            birthday={each.birthday}
-            gender={each.gender}
-            job={each.job}
-            />
-          )
-        })
-      }
-    </div>
-  );
+    return (
+      <div>
+        <MuiTable/>
+      </div>
+    );
 }
 
 export default App;
--- management/src/components/Customer.js
+++ management/src/components/Customer.js
@@ -1,35 +1,88 @@
 import React from 'react';
+import { Table,TableContainer,TableHead, TableBody, TableRow, TableCell, Paper, getTableHeadUtilityClass} from '@mui/material';
+
+
+const customers = [{
+  'id': 1,
+  'image': 'https://placeimg.com/64/64/1',
+  'name': 'Max',
+  'birthday': '1',
+  'gender': 'Male',
+  'job': 'Student'
+},
+{
+  'id': 2,
+  'image': 'https://placeimg.com/64/64/2',
+  'name': 'Emma',
+  'birthday': '2',
+  'gender': 'Female',
+  'job': 'Student'
+},
+{
+  'id': 3,
+  'image': 'https://placeimg.com/64/64/3',
+  'name': 'Van',
+  'birthday': '3',
+  'gender': 'Female',
+  'job': 'Student'
+},
+{
+  'id': 4,
+  'image': 'https://placeimg.com/64/64/4',
+  'name': 'Grief',
+  'birthday': '4',
+  'gender': 'Male',
+  'job': 'Student'
+},
+]
+
+
+export const MuiTable = () => {
+  return(
+    <TableContainer component={Paper}>
+      <Table aria-label='simpel table'>
+        <TableHead>
+          <TableRow>
+            <TableCell>ID</TableCell>
+            <TableCell>Image</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell>Birthday</TableCell>
+            <TableCell>Gender</TableCell>
+            <TableCell>Job</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {customers.map(row => (
+            <TableRow 
+              key = {row.id}
+              sx = {{'a':{border:0}}}
+           >
+            <TableCell>{row.id}</TableCell>
+            <TableCell>{row.image}</TableCell>
+            <TableCell>{row.birthday}</TableCell>
+            <TableCell>{row.gender}</TableCell>
+            <TableCell>{row.job}</TableCell> 
+           </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  )
+}
 
 
 class Customer extends React.Component{
-  render(){
-    return(
-    <div>
-      <CustomerProfile id={this.props.id} image={this.props.image} name={this.props.name}/>
-      <CustomerInfo birthday={this.props.birthday} gender={this.props.gender} job={this.props.job}/>
-    </div>
-    )
-  }
-}
-
-class CustomerProfile extends React.Component{
-  render(){
+  redner(){
     return(
       <div>
-        <img src={this.props.image} alt="profile"/>
-        <h2>{this.props.name}({this.props.id})</h2>
-      </div>
-    )
-  }
-}
-
-class CustomerInfo extends React.Component{
-  render(){
-    return(
-      <div>
-        <p>{this.props.birthday}</p>
-        <p>{this.props.gender}</p>
-        <p>{this.props.job}</p>
+      <TableRow>
+        <TableCell>{this.props.id}</TableCell>
+        <TableCell><img src={this.props.image} alt='profile'/></TableCell>
+        <TableCell>{this.props.name}</TableCell>
+        <TableCell>{this.props.birthday}</TableCell>
+        <TableCell>{this.props.gender}</TableCell>
+        <TableCell>{this.props.job}</TableCell>
+      </TableRow>
       </div>
     )
   }
